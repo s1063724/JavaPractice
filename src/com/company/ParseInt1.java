0bd29c4860b41ab17f535618a0be0ca84ccad963
Wuray <s1063724@gm.pu.edu.tw>
2020-11-13 15:14:43 +0800

    字串轉整數

--- src/com/company/ParseInt1.java
+++ src/com/company/ParseInt1.java
@@ -4,7 +4,8 @@ public class ParseInt1 {
     public static void main(String[]args){
         String str="22 56 75 48";
         String sp[]=str.split(" ");
-
+        System.out.println(sp[0]);
+        System.out.println("--------------------");
         for(String i:sp){
             System.out.println(Integer.parseInt(i));
         }
